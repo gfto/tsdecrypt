5d32fcf0a66c4cbeb463700003cca172f76c6994
Georgi Chorbadzhiyski <gf@unixsol.org>
2011-09-29 19:10:59 +0300

    Set new code code part only if it is valid.
    This fixes Sky Italia HD channels. When they send new
    code word the one that is about to expire is set to 0
    and this causes short decryption failures. Now if code
    word is all zeroes it is not set.

--- camd.c
+++ camd.c
@@ -174,11 +174,14 @@ READ:
 		c->key->ts = c->key->ts_keyset.tv_sec;
 		ts->cw_last_warn = c->key->ts;
 
-		dvbcsa_key_set(c->key->cw    , c->key->csakey[0]);
-		dvbcsa_key_set(c->key->cw + 8, c->key->csakey[1]);
-
-		dvbcsa_bs_key_set(c->key->cw    , c->key->bs_csakey[0]);
-		dvbcsa_bs_key_set(c->key->cw + 8, c->key->bs_csakey[1]);
+		if (memcmp(c->key->cw, invalid_cw, 8) != 0) {
+			dvbcsa_key_set   (c->key->cw, c->key->csakey[0]);
+			dvbcsa_bs_key_set(c->key->cw, c->key->bs_csakey[0]);
+		}
+		if (memcmp(c->key->cw + 8, invalid_cw, 8) != 0) {
+			dvbcsa_key_set(c->key->cw + 8, c->key->csakey[1]);
+			dvbcsa_bs_key_set(c->key->cw + 8, c->key->bs_csakey[1]);
+		}
 	}
 
 	if (ts->ecm_cw_log) {
