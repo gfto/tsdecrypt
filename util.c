76b533ae1067f70c40cebb77c092507812398098
Georgi Chorbadzhiyski <gf@unixsol.org>
2012-08-21 21:14:05 +0300

    Add getline() for challenged systems (OSX, Android).
    This fixes compilation under OS X.

--- util.c
+++ util.c
@@ -182,6 +182,64 @@ int64_t get_time(void) {
 	return ts.tv_sec * 1000000ll + ts.tv_nsec / 1000;
 }
 
+#if defined(__APPLE__) || defined(__ANDROID__)
+ssize_t getdelim(char **linep, size_t *np, int delim, FILE *stream)
+{
+	int ch;
+	size_t new_len;
+	ssize_t i = 0;
+	char *line, *new_line;
+
+	// Invalid input
+	if (!linep || !np) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	if (*linep == NULL || *np == 0) {
+		*np = 1024;
+		*linep = calloc(1, *np);
+		if (*linep == NULL)
+			return -1;
+	}
+	line = *linep;
+
+	while ((ch = getc(stream)) != EOF) {
+		if ((size_t)i > *np) {
+			// Need more space
+			new_len  = *np + 1024;
+			new_line = realloc(*linep, new_len);
+			if (!new_line)
+				return -1;
+			*np    = new_len;
+			*linep = new_line;
+		}
+
+		line[i] = ch;
+		if (ch == delim)
+			break;
+		i += 1;
+	}
+
+	if ((size_t)i > *np) {
+		// Need more space
+		new_len  = i + 2;
+		new_line = realloc(*linep, new_len);
+		if (!new_line)
+			return -1;
+		*np    = new_len;
+		*linep = new_line;
+	}
+	line[i + 1] = '\0';
+
+	return i > 0 ? i : -1;
+}
+
+ssize_t getline(char **linep, size_t *np, FILE *stream) {
+	return getdelim(linep, np, '\n', stream);
+}
+#endif
+
 unsigned int file_hex2buf(char *filename, uint8_t *buffer, unsigned int buf_size) {
 	FILE *f = fopen(filename, "r");
 	if (!f) {
