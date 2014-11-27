8c14716e5cff5f2f518803bfaea0826b08bb86bf
Georgi Chorbadzhiyski <gf@unixsol.org>
2014-11-28 01:44:16 +0200

    Let getopt() print error messages about options.
    This removes tsdecrypt's error message in favour of getopt
    native errors. Our message was always 'Unknown parameter'
    which was not correct (most of the time) and very misleading.

--- tsdecrypt.c
+++ tsdecrypt.c
@@ -306,12 +306,9 @@ extern int optind, opterr, optopt;
 
 static void parse_options(struct ts *ts, int argc, char **argv) {
 	int j, i, ca_err = 0, server_err = 1, input_addr_err = 0, output_addr_err = 0, ident_err = 0, port_set = 0;
-	opterr = 0; // Prevent printing of error messages for unknown options in getopt()
 	while ((j = getopt_long(argc, argv, short_options, long_options, NULL)) != -1) {
-		if (j == '?') {
-			fprintf(stderr, "ERROR: Unknown parameter '%s'.\n", argv[optind - 1]);
+		if (j == '?')
 			exit(EXIT_FAILURE);
-		}
 		switch (j) {
 			case 'i': // -- ident
 				ts->ident = optarg;
